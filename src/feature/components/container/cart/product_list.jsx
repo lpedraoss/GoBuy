48d6ca7a15c110bad3c3cd3d48f8624023a7e404
lpedraoss <alfonso.suarez.dev@gmail.com>
2023-06-02 13:53:35 -0500

    modified:     - para ti

--- src/feature/components/container/cart/product_list.jsx
+++ src/feature/components/container/cart/product_list.jsx
@@ -3,47 +3,43 @@ import CartContext from '../../../context/cart/cart_context';
 import { useNavigate } from 'react-router-dom';
 import Cart from './cart';
 
-
 const ProductList = () => {
-    const { addToCart } = useContext(CartContext);
+  const { addToCart } = useContext(CartContext);
 
-    // esto es un ejemplo de cuando traigo del api
-    const jsonProduct = '[{"id": 1, "name": "Producto 1", "price": 10, "image": "/src/assets/img/sombrero.webp"}, {"id": 2, "name": "Producto 2", "price": 20, "image": "/src/assets/img/carro.jpeg"}, {"id": 3, "name": "Producto 3", "price": 30, "image": "/src/assets/img/cicla.webp"}]';
+  // Esto es un ejemplo de cuando traigo del API
+  const jsonProduct =
+    '[{"id": 1, "name": "Producto 1", "price": 10, "image": "/src/assets/img/sombrero.webp", "quantity": 5}, {"id": 2, "name": "Producto 2", "price": 20, "image": "/src/assets/img/carro.jpeg", "quantity": 10}, {"id": 3, "name": "Producto 3", "price": 30, "image": "/src/assets/img/cicla.webp", "quantity": 3}]';
 
-    //lo transformo en un array de objetos
-    const products = JSON.parse(jsonProduct);
+  // Lo transformo en un array de objetos
+  const products = JSON.parse(jsonProduct);
 
-    const handleAddToCart = (productSelected) => {
-        addToCart(productSelected);
-    }
-    const route = useNavigate();
-    const handleGoCart = () => {
-      route('/cart');
-    }
-    return (
+  const handleAddToCart = (productSelected) => {
+    addToCart(productSelected);
+  };
 
-      <>
-        
-        <article>
-          <button onClick={handleGoCart} > ir a carrito</button>
-          {products.map((product) => (
-            <section key={product.id}>
-              <h3>{product.name}</h3>
-              <p>Precio: {product.price}</p>
-              <img src={product.image} alt={product.name} />
-              <button onClick={() => handleAddToCart(product)}>
-                Agregar al carrito
-              </button>
-            </section>
-            
-          ))}
-          <h2>---------------------carrito de compras ---------------------</h2>
-          <Cart></Cart>
-        </article>
-      </>
-        
+  const route = useNavigate();
+  const handleGoCart = () => {
+    route('/cart');
+  };
 
-    );
-}
-
+  return (
+    <>
+      <article>
+        <button onClick={handleGoCart}>Ir al carrito</button>
+        {products.map((product) => (
+          <section key={product.id}>
+            <h3>{product.name}</h3>
+            <p>Precio: {product.price}</p>
+            <img src={product.image} alt={product.name} />
+            <button onClick={() => handleAddToCart(product)}>
+              Agregar al carrito
+            </button>
+          </section>
+        ))}
+        <h2>--------------------- Carrito de compras ---------------------</h2>
+        <Cart />
+      </article>
+    </>
+  );
+};
 export default ProductList;
--- src/feature/context/cart/cart_provider.jsx
+++ src/feature/context/cart/cart_provider.jsx
@@ -1,46 +1,82 @@
 import  { useState } from 'react';
 import CartContext from './cart_context';
 
-const CartProvider = ({children}) => {
-    const [cartItems,setCartItems] = useState([]);
-     // Agrega un producto al carrito
-    const addToCart = ( product ) => {
-        setCartItems([...cartItems, product]);
-    }
-    const removeFromCart = ( productId ) => {
-        //filtra los elementos del carrito para eliminar el producto con el productId 
-        const updatedCartItems = cartItems.filter(
-            (item) => item.id !== productId
-        );
-        setCartItems(updatedCartItems);
-    }
-    //actualiza la cantidad de un producto en el carrito
-    const updateQuantity = (productId, newQuantity) => {
-        const updatedCartItems = cartItems.map(
-            (item) => {
-                if(item.id === productId){
-                    return {
-                        ...item, quantity: newQuantity
-                    }
-                }
-                return item;
-            }
-        )
-        setCartItems(updatedCartItems);
-    }
-    // Calcula el total de la compra
-    const calculateTotal = () => {
-        let total = 0;
-        cartItems.forEach((item)=>{
-            total += item.price * item.quantity; 
-        });
-        return total;
-    }
-    return (
-        <CartContext.Provider value={{cartItems, addToCart, removeFromCart, updateQuantity, calculateTotal}} >
-            {children}
-        </CartContext.Provider>
-    );
-}
+const CartProvider = ({ children }) => {
+  const [cartItems, setCartItems] = useState([]);
 
+  const addToCart = (product) => {
+    if (product.quantity > 0) {
+      const existingProduct = cartItems.find((item) => item.id === product.id);
+      if (existingProduct) {
+        if (existingProduct.quantity < product.quantity) {
+          setCartItems((prevItems) =>
+            prevItems.map((item) =>
+              item.id === product.id
+                ? { ...item, quantity: item.quantity + 1 }
+                : item
+            )
+          );
+        } else {
+          console.log("No se pueden agregar más unidades de este producto al carrito");
+        }
+      } else {
+        setCartItems((prevItems) => [...prevItems, { ...product, quantity: 1 }]);
+      }
+    } else {
+      console.log("No hay stock disponible de este producto");
+    }
+  };
+
+  const removeFromCart = (productId) => {
+    const updatedCartItems = cartItems.filter((item) => item.id !== productId);
+    setCartItems(updatedCartItems);
+  };
+
+  const updateQuantity = (productId, newQuantity) => {
+    const updatedCartItems = cartItems.map((item) => {
+      if (item.id === productId) {
+        if (newQuantity <= item.quantity) {
+          return {
+            ...item,
+            quantity: newQuantity,
+          };
+        } else {
+          console.log("No se pueden pedir más unidades de este producto de las disponibles");
+          return item;
+        }
+      }
+      return item;
+    });
+    setCartItems(updatedCartItems);
+  };
+
+  const updateStock = (productId, newStock) => {
+    const updatedCartItems = cartItems.map((item) => {
+      if (item.id === productId) {
+        return {
+          ...item,
+          stock: newStock,
+        };
+      }
+      return item;
+    });
+    setCartItems(updatedCartItems);
+  };
+
+  const calculateTotal = () => {
+    let total = 0;
+    cartItems.forEach((item) => {
+      total += item.price * item.quantity;
+    });
+    return total;
+  };
+
+  return (
+    <CartContext.Provider
+      value={{ cartItems, addToCart, removeFromCart, updateQuantity, updateStock, calculateTotal }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
 export default CartProvider;
