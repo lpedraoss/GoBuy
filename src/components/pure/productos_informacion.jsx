a351d5914403a66912cf9fa017bb75c0be3be261
lpedraoss <alfonso.suarez.dev@gmail.com>
2023-06-08 11:27:12 -0500

    modified:     - carrito de compras

--- src/components/pure/productos_informacion.jsx
+++ src/components/pure/productos_informacion.jsx
@@ -1,5 +1,5 @@
 
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import Cart from './carrito';
 
@@ -7,36 +7,40 @@ import CartContext from "../../feature/cart/context/carrito_context";
 import UseDataProduct from "../../data/product/use_data_product";
 
 const Informacion_producto = () => {
-  
 
   const { addToCart } = useContext(CartContext);
   const { productos } = UseDataProduct();
-  
-  if (productos.length === 0) {
+
+  const [productosActualizados, setProductosActualizados] = useState([]);
+
+  useEffect(() => {
+    setProductosActualizados(productos);
+  }, [productos]);
+
+  if (productosActualizados.length === 0) {
     return <h1>NO HAY DATOS</h1>;
   }
 
   const handleAddToCart = (productSelected) => {
-    addToCart(productSelected);
+    const updatedProduct = { ...productSelected, quantityToBuy: 1 };
+    addToCart(updatedProduct);
   };
 
   return (
-  <article id="contenedor_producto">
-    <ul id="productos">
-      {productos.map((dato) => (
-        <li key={dato.id}>
-          <h2>{dato.nombre}</h2>
-          <img className="img-producto" src={dato.imagen} alt={dato.nombre} />
-          <p className="descripcion">{dato.descripcion}</p>
-          <span className="precio"> $ {dato.precio}</span>
-          <button className="agregar-carrito" onClick={() => handleAddToCart(dato)} > Agregar al carrito </button>
-        </li>
-      ))}
-    </ul>
-
-<Cart/>
-
-</article>
+    <article id="contenedor_producto">
+      <ul id="productos">
+        {productosActualizados.map((dato) => (
+          <li key={dato.id}>
+            <h2>{dato.nombre}</h2>
+            <img className="img-producto" src={dato.imagen} alt={dato.nombre} />
+            <p className="descripcion">{dato.descripcion}</p>
+            <span className="precio"> $ {dato.precio}</span>
+            <button className="agregar-carrito" onClick={() => handleAddToCart(dato)}>Agregar al carrito</button>
+          </li>
+        ))}
+      </ul>
+      <Cart />
+    </article>
   );
 };
 
